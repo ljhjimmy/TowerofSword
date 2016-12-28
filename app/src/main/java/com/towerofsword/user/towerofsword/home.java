480b2bbe386db6a780627ef88f85fd082d6c045f
ljhjimmy <ljhjimmy87@gmail.com>
2016-12-29 01:04:04 +0800

    123

--- app/src/main/java/com/towerofsword/user/towerofsword/home.java
+++ app/src/main/java/com/towerofsword/user/towerofsword/home.java
@@ -1,7 +1,9 @@
 package com.towerofsword.user.towerofsword;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+            new AlertDialog.Builder(Home.this)
+                    .setTitle("確認視窗")
+                    .setMessage("確定要結束應用程式嗎?")
+                    //.setIcon(R.drawable.ic_menu_info_details)
+                    .setPositiveButton("確定",
+                            new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                                    int which) {
+                                    finish();
+                                }
+                            })
+                    .setNegativeButton("取消",
+                            new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                                    int which) {
+                                }
+                            }).show();
     }
 }
