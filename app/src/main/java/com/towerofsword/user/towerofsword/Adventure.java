60fe8ada15b944ad3cff85c798cba6d1157d8e21
ljhjimmy <ljhjimmy87@gmail.com>
2017-01-01 18:03:18 +0800

    add adventure.xml

---
+++ app/src/main/java/com/towerofsword/user/towerofsword/Adventure.java
@@ -0,0 +1,18 @@
+package com.towerofsword.user.towerofsword;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+public class Adventure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adventure);
+    }
+
+}
+
+
--- app/src/main/java/com/towerofsword/user/towerofsword/Home.java
+++ app/src/main/java/com/towerofsword/user/towerofsword/Home.java
@@ -1,9 +1,12 @@
 package com.towerofsword.user.towerofsword;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -11,7 +14,22 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btnAdventure = (Button)findViewById(R.id.btnAdventure);
+        btnAdventure.setOnClickListener(listenerAdventure);
     }
+
+
+    private Button.OnClickListener listenerAdventure = new Button.OnClickListener(){
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(Home.this, Adventure.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
+            overridePendingTransition(0,0);
+            finish();
+        }
+    };
 
     @Override
     public void onBackPressed() {
